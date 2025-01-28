209e9ade96c28427e9a87fdb67a16ce93f708283
kshitijmandle <kshitijmandle703044@gmail.com>
2025-01-28 23:38:07 +0530

    28 Jan 2025

---
+++ parrotc/practicals/strings/str-pr1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+void main () {
+
+char c_string[20];
+char s_string[20];
+
+printf("Enter c string : \n");
+scanf("%s",&c_string);
+
+printf("%s",c_string);
+
+
+}
